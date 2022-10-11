28a74f9b92f702ab2933bea2605dc510d6281a31
Vira <vira.avramenko@gmail.com>
2022-10-10 20:30:21 -0400

    Palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string strName)
+        {
+            char[] cArray = strName.ToLower().ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+
+            if (strName.ToLower() == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;   
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,15 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void Test1(string strName, bool expected)
         {
+            var wordSmith = new WordSmith();
+            var actual = wordSmith.IsAPalindrome(strName);
+            Assert.Equal(expected, actual);
 
         }
     }
